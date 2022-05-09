43422c8c45117b8120596477a07017e2346b051f
Linus Behrbohm <linusbehrbohm@web.de>
2022-05-09 19:55:05 +0200

    Control logging through features

--- seqraph/src/logger.rs
+++ seqraph/src/logger.rs
@@ -1,21 +1,49 @@
 use tracing_appender::non_blocking::WorkerGuard;
-use tracing_subscriber::{fmt::*, layer::SubscriberExt,  EnvFilter};
+use tracing_subscriber::{
+    fmt::{
+        self,
+        writer::MakeWriterExt,
+        format::PrettyFields,
+    },
+    field::MakeExt,
+    layer::SubscriberExt,
+    util::SubscriberInitExt,
+};
+use tracing::Level;
 
 
-pub struct Logger(WorkerGuard);
+pub struct Logger;
 
 
 impl Default for Logger {
     fn default() -> Self {
-        let dir = tempfile::tempdir().expect("Failed to create tempdir");
-        let file_appender = tracing_appender::rolling::hourly(dir, "hypergraph");
-        let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
+        let registry = tracing_subscriber::registry();
 
-        let collector = tracing_subscriber::registry()
-            .with(EnvFilter::from_default_env().add_directive(tracing::Level::TRACE.into()))
-            .with(Layer::new().with_writer(std::io::stdout))
-            .with(Layer::new().with_writer(non_blocking));
-        tracing::subscriber::set_global_default(collector).expect("Unable to set a global collector");
-        Logger(guard)
+        #[cfg(feature = "log_file")]
+        let registry = {
+            let dir = tempfile::tempdir().expect("Failed to create tempdir");
+            let file_appender = tracing_appender::rolling::hourly(dir, "hypergraph");
+            let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+            let file_writer = non_blocking
+                .with_max_level(Level::TRACE);
+            let file_layer = fmt::layer()
+                .with_writer(file_writer)
+                .pretty();
+            registry.with(file_layer)
+        };
+
+        #[cfg(feature = "log_stdout")]
+        let registry = {
+            let stdout_writer = std::io::stdout
+                .with_max_level(Level::DEBUG);
+            let stdout_layer = fmt::layer()
+                .with_writer(stdout_writer)
+                .pretty()
+                .fmt_fields(PrettyFields::new().debug_alt());
+            registry.with(stdout_layer)
+        };
+
+        registry.init();
+        Logger
     }
 }
